1b962896a9d3f4f9e4017a3e71b1a33737a349d3
Dorian Rahamim <drahamim@greencheckverified.com>
2018-08-11 13:41:59 -0400

    Adds pin entry phase

--- src/App.js
+++ src/App.js
@@ -5,11 +5,106 @@ import './App.css';
 
 class App extends Component {
 
-  onNameContinue(event) {
-    console.log(event);
+  constructor(props) {
+    super(props);
+    this.state = {
+      phase: 'nameEntry',
+      firstName: '',
+      lastName: '',
+      pin: ''
+    };
+
+    this.handleFirstNameChange = this.handleChange.bind(this, 'firstName');
+    this.handleLastNameChange = this.handleChange.bind(this, 'lastName');
+    this.handlePinChange = this.handleChange.bind(this, 'pin');
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(key, event) {
+    let stateUpdate = {};
+    stateUpdate[key] = event.target.value;
+    this.setState(stateUpdate);
+  }
+
+  handleSubmit(event) {
+    if(this.state.phase === 'nameEntry') {
+      this.setState({'phase': 'pinEntry'});
+    }
+    event.preventDefault();
+  }
+
+  nameEntryElements() {
+    return [
+      <input key="firstName" type="text" placeholder="First name"
+        value={this.state.firstName} onChange={this.handleFirstNameChange}
+        style={{
+          "padding": "10px",
+          "fontSize": "26px",
+          "width": "350px",
+          "margin": "10px"
+        }}/>,
+      <input key="lastName" type="text" placeholder="Last name"
+        value={this.state.lastName} onChange={this.handleLastNameChange}
+        style={{
+          "padding": "10px",
+          "fontSize": "26px",
+          "width": "350px",
+          "margin": "10px"
+        }}/>,
+      <div key="submit"
+        style={{
+          "padding": "14px",
+          "width": "350px",
+          "margin": "10px",
+          "backgroundColor": "#005ea6",
+          "color": "white",
+          "fontSize": "22px",
+          "borderRadius": "15px",
+          "cursor":"pointer"
+        }}
+        onClick = {this.handleSubmit}>
+          Continue
+      </div>
+    ];
+  }
+
+  pinEntryElements() {
+    return [
+      <input key="pin" type="text" placeholder="4-digit PIN"
+        value={this.state.value} onChange={this.handlePinChange}
+        style={{
+          "padding": "10px",
+          "fontSize": "26px",
+          "width": "350px",
+          "margin": "10px"
+        }}/>,
+      <div key="submit"
+        style={{
+          "padding": "14px",
+          "width": "350px",
+          "margin": "10px",
+          "backgroundColor": "#005ea6",
+          "color": "white",
+          "fontSize": "22px",
+          "borderRadius": "15px",
+          "cursor":"pointer"
+        }}
+        onClick = {this.handleSubmit}>
+          Withdraw Delicious Cookies
+      </div>
+    ];
   }
 
   render() {
+    let formElements = [];
+    if(this.state.phase === 'nameEntry') {
+      formElements = this.nameEntryElements()
+    } else if(this.state.phase === 'pinEntry') {
+      debugger;
+      formElements = this.pinEntryElements()
+    }
+
+
     return (
       <div className="App" style={{
         "display": "flex",
@@ -46,36 +141,10 @@ class App extends Component {
           "flexDirection": "column",
           "justifyContent": "center",
           "alignItems": "center",
-          "display": "flex"
+          "display": "flex",
+          "textAlign": "center"
         }}>
-          <input type="text" name="firstName" placeholder="First name"
-            style={{
-              "padding": "10px",
-              "fontSize": "26px",
-              "width": "350px",
-              "margin": "10px"
-            }}/>
-          <input type="text" name="lastName" placeholder="Last name"
-            style={{
-              "padding": "10px",
-              "fontSize": "26px",
-              "width": "350px",
-              "margin": "10px"
-            }}/>
-          <div
-            style={{
-              "padding": "14px",
-              "width": "350px",
-              "margin": "10px",
-              "backgroundColor": "#005ea6",
-              "color": "white",
-              "fontSize": "22px",
-              "borderRadius": "15px",
-              "cursor":"pointer"
-            }}
-            onClick = {this.onNameContinue}>
-              Continue
-            </div>
+          {formElements}
         </div>
       </div>
     );
